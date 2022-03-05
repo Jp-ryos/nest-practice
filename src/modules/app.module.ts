158e49a7a6a4e8f98060ff1cf336e21e2241d074
ryoccd <r.shibata1001@gmail.com>
2022-03-05 21:36:14 +0900

    fix: resolve complile error

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,17 +1,9 @@
 import { Module } from '@nestjs/common';
-import { APP_SERVICE } from '../services/app-service.interface';
-import { AppController } from '../controllers/app.controller';
-import { AppService } from '../services/implementation/app.service';
 
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [
-    {
-      provide: APP_SERVICE,
-      useClass: AppService
-    }
-  ],
+  controllers: [],
+  providers: []
 })
 export class AppModule { }
