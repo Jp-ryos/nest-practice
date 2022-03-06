9b90346d131ab3df1ff780c9c1cd52408d132bb4
ryoccd <r.shibata1001@gmail.com>
2022-03-07 05:41:43 +0900

    rename&move: move and rename app.module.ts

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      autoLoadEntities: true
+    }),
+    UserModule,
+    AuthModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
