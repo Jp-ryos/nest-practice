2e21ac544ec74ec75d5c21b2c2f6c6823c42abb8
ryoccd <r.shibata1001@gmail.com>
2022-03-07 05:40:59 +0900

    add: add userCreateDto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,9 @@
+import { CreateUserRequestV1 } from '../models/request/user-createrequest.model';
 
+export class CreateUserDto {
+  constructor(createUserRequest: CreateUserRequestV1) {
+    this.request = createUserRequest;
+  }
+
+  request: CreateUserRequestV1;
+}
