41095a2a60ae2a2f8fe7d6a9dbde3aff52c06a4d
ryoccd <r.shibata1001@gmail.com>
2022-03-07 05:42:18 +0900

    update: update validation userUpdateRequest model

--- src/user/models/request/user-updaterequest.model.ts
+++ src/user/models/request/user-updaterequest.model.ts
@@ -1,8 +1,13 @@
-import { IsEmail, MaxLength } from 'class-validator';
+import { MaxLength, ValidateIf, IsEmail } from 'class-validator';
 
 export class UpdateUserRequestV1 {
 
+  @ValidateIf((o, v) => v != null && v.length)
+  @MaxLength(20)
   userName: string | null;
 
+
+  @ValidateIf((o, v) => v != null && v.length)
+  @IsEmail()
   emailAddress: string | null;
 }
