0c96bc4513e2e8d45a629ed9ec7e463b50bd3c04
jp-ryos <ubeat1001@gmail.com>
2022-03-08 09:51:28 +0900

    update: update common module
    * detail
    - TimeProvider
    -- get zonedDateTime of ISO_8601 format

--- src/common/services/TimeProvider.ts
+++ src/common/services/TimeProvider.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { formatToTimeZone } from 'date-fns-timezone';
 
 export const _privateId_timeProvider: string = '94c723c4-b08f-40db-b087-b39afd8afad9';
 @Injectable()
 export class TimeProvider {
 
   currentDateTime(): string {
-    return new Date().toISOString();
+    return formatToTimeZone(new Date(), 'YYYY-MM-DDTHH:mm:ssZ', { timeZone: 'Asia/Tokyo' });
   }
 }
